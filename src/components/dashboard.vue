6b3b0ce12010a54bc700ff6102cf9cd125d841a3
KomalDesai <kdesai9895@gmail.com>
2022-08-27 02:42:42 +0530

    dashboard display

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -2,8 +2,25 @@
   <div class="container mt-5 p-5">
     <div class="row row-cols-3">
       <div class="col border">Number of leaves applied</div>
-      <div class="col border">Number of Sick leaves</div>
-      <div class="col border">Number of Annual leaves</div>
+      <div class="row row-cols-2">
+        <div class="col border">Number of Sick leaves pending</div>
+
+        <div class="col border">Number of Annual leaves pending</div>
+        <div class="col border">
+          <div class="row row-cols-2">
+            Number of Sick leaves
+            <div class="col border">approved</div>
+            <div class="col border">rejected</div>
+          </div>
+        </div>
+        <div class="col border">
+          <div class="row row-cols-2">
+            Number of Annual leaves
+            <div class="col border">approved</div>
+            <div class="col border">rejected</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,8 +28,7 @@
 <script>
 export default {
   name: "LeaveDashboard",
-   methods:{
-       }
+  methods: {},
 };
 </script>
 
